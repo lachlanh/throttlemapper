73f798d22a560c6dc3f32bae64a84f67a7ddd46c
lachlanh <lhespe@opentable.com>
2018-11-28 22:36:50 -0800

    more tweaks still not working

--- vesc_uart_test.ino/vesc_uart_test.ino.ino
+++ vesc_uart_test.ino/vesc_uart_test.ino.ino
@@ -1,32 +1,34 @@
-
+c
 
 #include <VescUart.h>
 
 /** Initiate VescUart class */
 VescUart UART;
 
-float current = 25.0; /** The current in amps */
+float current_on = 100.0; /** The current in amps */
+float current_off = 0.0;
 const int ledPin = 17;
 
 void setup() {
   Serial.begin(9600);
   /** Setup UART port (Serial1 on Atmega32u4) */
-  Serial1.begin(115200);
+  Serial1.begin(19200);
   
   while (!Serial1) {;}
 
   /** Define which ports to use as UART */
   UART.setSerialPort(&Serial1);
+  UART.setDebugPort(&Serial);
 }
 
 void loop() {
   if ( UART.getVescValues() ) {
-    Serial.println("print vesc values");
-    Serial.println(UART.data.rpm);
-    Serial.println(UART.data.inpVoltage);
-    Serial.println(UART.data.ampHours);
-    Serial.println(UART.data.tachometerAbs);
-
+//    Serial.println("print vesc values");
+//    Serial.println(UART.data.rpm);
+//    Serial.println(UART.data.inpVoltage);
+//    Serial.println(UART.data.ampHours);
+//    Serial.println(UART.data.tachometerAbs);
+    UART.printVescValues();
   }
   else
   {
@@ -34,31 +36,31 @@ void loop() {
   }
   
   /** Call the function setCurrent() to set the motor current */
-  UART.setCurrent(current);
+  UART.setCurrent(current_on);
 
   digitalWrite(ledPin, LOW);
 
-  delay(1000);
+  delay(100);
 
-  UART.setCurrent(0.0);
-
-  digitalWrite(ledPin, HIGH);
-
-  delay(1000);
-
-  UART.setDuty(0.5);
-
-  delay(1000);
-
-  UART.setDuty(0.0);
-
-  delay(1000);
-
-  UART.setRPM(1000);
-
-  delay(1000);
-
-  UART.setRPM(0);
+//  UART.setCurrent(0.0);
+//
+//  digitalWrite(ledPin, HIGH);
+//
+//  delay(1000);
+//
+//  UART.setDuty(0.5);
+//
+//  delay(1000);
+//
+//  UART.setDuty(0.0);
+//
+//  delay(1000);
+//
+//  UART.setRPM(1000);
+//
+//  delay(1000);
+//
+//  UART.setRPM(0);
 
   
   
